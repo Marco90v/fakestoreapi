1f27e2aa1e48afe6cb1da356b6b6c619b5fa445c
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2022-06-06 08:22:41 -0400

    add navlink login or user

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,12 @@ import { userContext } from "../context/Context";
 
 const linkActiveStyle = {
     borderBottom: "1px solid black",
+    fontWeigh: "bold"
 };
 
 const Header = () => {
+    // console.log('Header');
+
     const { state } = useContext(userContext);
     const [toggle, setToggle] = useState(false);
     const [width, setWidth] = useState(window.innerWidth);
@@ -47,7 +50,19 @@ const Header = () => {
                         </li>
                     )
                 }
-                <li><NavLink to="Login" style={ linkActive }>Login</NavLink></li>
+                {
+                    state.user ?
+                    <li>
+                        <NavLink to="user" style={ linkActive }>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                            </svg> User
+                        </NavLink>
+                    </li>
+                    :
+                    <li><NavLink to="Login" style={ linkActive }>Login</NavLink></li>
+                }
             </ul>
         </header>
     );
